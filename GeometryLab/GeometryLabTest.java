d35f06e715b4864170ef9903cb65a06ac7a0dec3
zolboo99 <programming.zolboo@gmail.com>
2015-08-27 17:53:58 +0000

    mah commet

---
+++ GeometryLab/GeometryLabTest.java
@@ -0,0 +1,7 @@
+public class GeometryLabTest {
+    public static void main (String[]args){
+        GeometryLab geo = new GeometryLab();
+        System.out.println(geo.f(geo.g(3,5), geo.k(7,9)));
+        System.out.println(geo.triArea(2, 3));
+    }
+}
